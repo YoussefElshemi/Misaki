c9b97631d63648c7116d97d97abd2060e26db48d
YoussefElshemi <33766341+YoussefElshemi@users.noreply.github.com>
2018-03-04 22:55:09 +0000

    Update volume.js

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -1,6 +1,5 @@
 /* eslint linebreak-style: 0 */
 const Command = require(`${process.cwd()}/base/Command.js`);
-const embed = require("../../modules/Embeds.js");
 
 class Volume extends Command {
   constructor(client) {
@@ -8,21 +7,20 @@ class Volume extends Command {
       name: "volume",
       description: "This command will set the volume of the songs.",
       usage: "volume [number]",
-      category: "Music",
-      permLevel: "Administrator"
+      category: "Music"
     });
   }
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
     const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) return embed("noVoiceChannel", message);
-    if (!this.client.playlists.has(message.guild.id)) return embed("emptyQueue", message);
-    if (!args[0]) return embed("currentVolume", message);
-    if (Number(args[0]) < 0 || Number(args[0]) > 100) return embed("errorVolume", message);
+    if (!voiceChannel) return this.client.embed("noVoiceChannel", message);
+    if (!this.client.playlists.has(message.guild.id)) return this.client.embed("emptyQueue", message);
+    if (!args[0]) return this.client.embed("currentVolume", message);
+    if (Number(args[0]) < 0 || Number(args[0]) > 100) return this.client.embed("errorVolume", message);
     message.guild.voiceConnection.volume = Number(args[0]) / 100;
     this.client.playlists.get(message.guild.id).volume = Number(args[0]);
     this.client.playlists.get(message.guild.id).connection.dispatcher.setVolumeLogarithmic(Number(args[0]) / 100);
-    return embed("volumeSet", message, args);
+    return this.client.embed("volumeSet", message, args);
   }
 }
 
