eeba6d1e0be1d429d227d52a0b4834c5601ae5a2
YoussefElshemi <33766341+YoussefElshemi@users.noreply.github.com>
2018-03-04 22:54:24 +0000

    Update stop.js

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -1,6 +1,5 @@
 /* eslint linebreak-style: 0 */
 const Command = require(`${process.cwd()}/base/Command.js`);
-const embed = require("../../modules/Embeds.js");
 
 class Stop extends Command {
   constructor(client) {
@@ -15,12 +14,12 @@ class Stop extends Command {
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
     const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) return embed("noVoiceChannel", message);
-    if (!this.client.playlists.has(message.guild.id)) return embed("emptyQueue", message);
+    if (!voiceChannel) return this.client.embed("noVoiceChannel", message);
+    if (!this.client.playlists.has(message.guild.id)) return this.client.embed("emptyQueue", message);
     const thisPlaylist = this.client.playlists.get(message.guild.id);
     thisPlaylist.songs = [];
     thisPlaylist.connection.dispatcher.end();
-    return embed("stopped", message);
+    return this.client.embed("stopped", message);
   }
 }
 
