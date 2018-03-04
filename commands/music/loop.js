da8462b49f41f592a62bc6e2bfb96a922ac11aa1
YoussefElshemi <33766341+YoussefElshemi@users.noreply.github.com>
2018-03-04 22:48:52 +0000

    Update loop.js

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -1,6 +1,5 @@
 /* eslint linebreak-style: 0 */
 const Command = require(`${process.cwd()}/base/Command.js`);
-const embed = require("../../modules/Embeds.js");
 
 class Loop extends Command {
   constructor(client) {
@@ -16,14 +15,14 @@ class Loop extends Command {
   async run(message, args, level) { // eslint-disable-line no-unused-vars
     const voiceChannel = message.member.voiceChannel;
     const thisPlaylist = this.client.playlists.get(message.guild.id);
-    if (!voiceChannel) return embed("noVoiceChannel", message);
-    if (!this.client.playlists.has(message.guild.id)) return embed("emptyQueue", message);
+    if (!voiceChannel) return this.client.embed("noVoiceChannel", message);
+    if (!this.client.playlists.has(message.guild.id)) return this.client.embed("emptyQueue", message);
     if (thisPlaylist.loop) {
       thisPlaylist.loop = false;
-      return embed("unloopedEmbed", message);
+      return this.client.embed("unloopedEmbed", message);
     } else {
       thisPlaylist.loop = true;
-      return embed("loopedEmbed", message);
+      return this.client.embed("loopedEmbed", message);
     }
   }
 }
