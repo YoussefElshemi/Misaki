774b6432c2f90633804e83909eff602d993904e1
YoussefElshemi <33766341+YoussefElshemi@users.noreply.github.com>
2018-03-04 22:54:10 +0000

    Update skip.js

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -1,6 +1,5 @@
 /* eslint linebreak-style: 0 */
 const Command = require(`${process.cwd()}/base/Command.js`);
-const embed = require("../../modules/Embeds.js");
 
 class Skip extends Command {
   constructor(client) {
@@ -9,18 +8,18 @@ class Skip extends Command {
       description: "This command will skip a current playing song.",
       usage: "skip",
       category: "Music",
-      aliases: ["next"],
+      aliases: ["next"]
     });
   }
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
     const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) return embed("noVoiceChannel", message);
-    if (!this.client.playlists.has(message.guild.id)) return embed("emptyQueue", message);
+    if (!voiceChannel) return this.client.embed("noVoiceChannel", message);
+    if (!this.client.playlists.has(message.guild.id)) return this.client.embed("emptyQueue", message);
     const thisPlaylist = this.client.playlists.get(message.guild.id);
     thisPlaylist.loop = false;
     thisPlaylist.connection.dispatcher.end("skip");
-    return embed("stopped", message);
+    return this.client.embed("stopped", message);
   }
 }
 
